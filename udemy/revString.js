4ffda2e8c266f8dad31a2c14865d755c998037d0
madhusudhanrao beesetty <madhusudhan@madhusudhan-2.local>
2018-05-12 20:11:09 -0700

    reduce funtion

--- udemy/revString.js
+++ udemy/revString.js
@@ -9,3 +9,12 @@ const fun = (str) => {
 
 fun('madhu');
 
+// reverse using reduce 
+
+const fun1 = (str1) => {
+  return str1.split('').reduce((returnStr,v)=>{
+  return v+returnStr;
+  },'');
+}
+
+console.log(fun1('madhusudhan'));
