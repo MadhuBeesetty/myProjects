7fb76520c2b0e183b1e7eb05fcf3c964032fe09f
madhusudhanrao beesetty <madhusudhan@madhusudhan-2.local>
2018-05-12 23:35:42 -0700

    creating subarrays

--- subarrays.js
+++ subarrays.js
@@ -11,3 +11,13 @@ const fun = (arr, num, i=0, arr1 = [], arr2 = []) => {
 }
 
 console.log(fun([1,2,3,4,5,6],2));
+
+// using splice
+
+const fun1 = (arr, num, arr1=[]) => {
+if(arr.length == 0) return arr1;
+arr1.push(arr.splice(0,num));
+return fun1 (arr,num,arr1);
+}
+
+console.log(fun1([1,2,3,4,5,6],2));
